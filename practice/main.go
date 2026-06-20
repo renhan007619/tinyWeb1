69368f1849dcd2dd96649ba7fbcdad48d0548ca2
renhan007619 <1473956205@qq.com>
2026-06-20 21:06:20 +0800

    2026-06-20: 持续学习，稳步前行

--- practice/main.go
+++ practice/main.go
@@ -2,31 +2,62 @@ package main
 
 import "fmt"
 
-var contacts = make(map[string]string) //注意要make()
-func addOrUpdateContact(phone string, name string) string { //注意后面还有返回值类型
-	for k, v := range contacts {
-		if k == phone {
-			contacts[k] = name + "更新"
-			return "更新成功"
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func convertBST(root *TreeNode) *TreeNode {
+	var pre *TreeNode
+	var traverse func(root *TreeNode)
+	traverse = func(node *TreeNode) {
+		if node == nil {
+			return
 		}
+		traverse(node.Right)
+		if pre != nil {
+			node.Val = node.Val + pre.Val
+		}
+		pre = node
+		traverse(node.Left)
 	}
-	contacts[phone] = name
-	return "新增成功"
+	traverse(root)
+	return root
+}
+func print(root *TreeNode) {
+	if root == nil {
+		return
+	}
+	leftVal := "nil"
+	rightVal := "nil"
+	if root.Left != nil {
+		leftVal = fmt.Sprintf("%d", root.Left.Val)
+	}
+	if root.Right != nil {
+		rightVal = fmt.Sprintf("%d", root.Right.Val)
+	}
+	fmt.Printf("节点 %d: 左=%s, 右=%s\n", root.Val, leftVal, rightVal)
+	print(root.Left)
+	print(root.Right)
 }
 func main() {
-	// 2026-06-03: 保持GitHub贡献活跃
-	// 2026-06-04: 每日学习打卡
-	// 2026-06-07: 继续打卡，加油！
-	// 2026-06-09: 坚持就是胜利
-	// 2026-06-10: 每日进步一点点
-	// 2026-06-11: 持之以恒，终有所成
-	// 2026-06-12: 积跬步以至千里
-	// 2026-06-13: 脚踏实地，仰望星空
-	// 2026-06-14: 代码练习，温故知新
-	// 2026-06-15: 勤学不辍，日新月异
-	// 2026-06-16: 精益求精，更进一步
-	// 2026-06-17: 持续积累，稳步前行
-	// 2026-06-18: 行稳致远，进而有为
-	fmt.Println(addOrUpdateContact("13800138000", "Alice"))
-	fmt.Println(addOrUpdateContact("13800138000", "Bob"))
+	// 2026-06-20: 持续学习，稳步前行
+	// 简单测试：  5
+	//           /
+	//          2
+	root := &TreeNode{
+		Val: 5,
+		Left: &TreeNode{
+			Val: 2,
+		},
+	}
+
+	fmt.Println("=== 转换前 ===")
+	print(root)
+
+	convertBST(root)
+
+	fmt.Println("\n=== 转换后 ===")
+	print(root)
 }
