6c0c4cfb3aa4f864cb4d2621e6fc713d8fbd4137
renhan007619 <1473956205@qq.com>
2026-06-26 22:55:08 +0800

    2026-06-26: 持之以恒，终有所成

--- practice/main.go
+++ practice/main.go
@@ -2,42 +2,24 @@ package main
 
 import "fmt"
 
-func combinationSum3(k int, n int) [][]int {
-	if (1+k)*k/2 > n || (19-k)*k/2 < n {
-		return nil
-		//边界条件写错了
+func point(s string, pointnum int) string {
+
+	s1 := make([]byte, len(s)+1)
+	for i := 0; i < pointnum; i++ {
+		s1[i] = s[i]
 	}
-	ret := [][]int{}
-	temp := []int{}
-	var c int = n
-	var dfs func(int)
-	dfs = func(cur int) {
-		if c < cur {
-			return
-		}
-		if c == 0 && len(temp) == k {
-			//len(temp)没有判断
-			c = n
-			comb := make([]int, k)
-			copy(comb, temp)
-			ret = append(ret, comb)
-			return
-		}
-		temp = append(temp, cur)
-		c -= cur
-		dfs(cur + 1)
-		temp = temp[0 : len(temp)-1]
-		c += cur //撤销后没有恢复c
-		dfs(cur + 1)
+	s1[pointnum] = '.'
+	for j := pointnum + 1; j < len(s)+1; j++ {
+		s1[j] = s[j-1]
 	}
-	dfs(1) //没有调用只定义了
-	fmt.Sprintf("%s", ret)
-	return ret
+	return string(s1)
+
 }
+
 func main() {
-	// 2026-06-21: 锲而不舍，金石可镂
-	// 2026-06-22: 学而不厌，诲人不倦
-	// 2026-06-23: 温故知新，知行合一
-	// 2026-06-24: 日积月累，厚积薄发
-	fmt.Println(combinationSum3(3, 7))
+	// 2026-06-26: 持之以恒，终有所成
+	s := "hello"
+	s1 := point(s, 2)
+	fmt.Printf("原字符串:%s\n", s)
+	fmt.Printf("函数后字符串:%s\n", s1)
 }
